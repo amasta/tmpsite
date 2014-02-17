e4e679cd7ed12df65b591636ceef9e5c5c9c4f73
Andy Mast <andy@cyngn.com>
2014-02-17 13:43:51 -0800

    Initial commit

---
+++ details.php
@@ -0,0 +1,16 @@
+{
+"title": "Evolution of Walter White",
+"author": "Matt Pascual",
+"image_urls": [ "static/wallpapers/1_full.png",
+             "static/wallpapers/1_full_b.png",
+             "static/wallpapers/1_full_c.png"
+           ],
+"price": "Free",
+"description": "Umbra, is a rounded icon pack for Android Launchers. With a light long shadow and a slight texture, Umbra is osmething different. In version 1.2.5, Umbra has over 690 handcrafted icons, 9 wallpapers in different styles, a Dashboard application and much more.",
+"details": [ "690+ Icons",
+             "15 fonts",
+             "9 Wallpapers",
+             "Custom Lockscreen",
+             "3rd party color packs"
+           ]
+}
---
+++ index.php
@@ -0,0 +1,25 @@
+{"items": [
+    {
+        "title": "Evolution of Walter White",
+        "author": "Matt Pascual",
+        "image_url": "/static/wallpapers/1.png",
+        "price": "Free",
+        "pkgName": "com.rovio.angrybirds"
+    },
+    {
+        "title": "Low Poly Tree",
+        "author": "AngryFish11",
+        "image_url": "/static/wallpapers/2.png",
+        "price": "Free",
+        "pkgName": "com.rovio.angrybirds"
+    },
+    {
+        "title": "Windoge 8",
+        "author": "RandomIraqi",
+        "image_url": "/static/wallpapers/3.png",
+        "price": "Free",
+        "pkgName": "com.rovio.angrybirds"
+    }
+],
+"next": "10_10" }
+
